243874dccd0af6c81e0342ecd8e84e9db2b76902
matty <mattreed1890@gmail.com>
2023-04-12 02:59:58 -0500

    expressions

--- src/parser.rs
+++ src/parser.rs
@@ -15,87 +15,112 @@ impl Parser {
         }
     }
 
-    fn expression(&mut self) -> Expr {
+    fn parse(&mut self) -> Option<Expr> {
+        let expr = self.expression();
+        match expr {
+            Ok(e) => Some(e),
+            Err(_) => None,
+        }
+    }
+
+    fn expression(&mut self) -> Result<Expr, ParseError> {
         self.equality()
     }
 
-    fn equality(&mut self) -> Expr {
-        let mut expr = self.comparison();
+    fn equality(&mut self) -> Result<Expr, ParseError> {
+        let mut expr = self.comparison()?;
 
         while self.match_op(vec![TokenType::BANG_EQ, TokenType::EQ_EQ]) {
             let op = self.previous();
-            let right = self.comparison();
+            let right = self.comparison()?;
             expr = Expr::Binary(Box::new(expr), Box::new(op), Box::new(right));
         }
 
-        expr
+        Ok(expr)
     }
 
-    fn comparison(&mut self) -> Expr {
-        let mut expr = self.term();
+    fn comparison(&mut self) -> Result<Expr, ParseError> {
+        let mut expr = self.term()?;
 
         while self.match_op(vec![TokenType::GT, TokenType::GT_EQ, TokenType::LT, TokenType::LT_EQ]) {
             let op = self.previous();
-            let right = self.term();
+            let right = self.term()?;
             expr = Expr::Binary(Box::new(expr), Box::new(op), Box::new(right));
         }
 
-        expr
+        Ok(expr)
     }
 
-    fn term(&mut self) -> Expr {
-        let mut expr = self.factor();
+    fn term(&mut self) -> Result<Expr, ParseError> {
+        let mut expr = self.factor()?;
 
         while self.match_op(vec![TokenType::MINUS, TokenType::PLUS]) {
             let op = self.previous();
-            let right = self.unary();
+            let right = self.unary()?;
             expr = Expr::Binary(Box::new(expr), Box::new(op), Box::new(right));
         }
  
-        expr
+        Ok(expr)
     }
 
-    fn factor(&mut self) -> Expr {
-        let mut expr = self.unary();
+    fn factor(&mut self) -> Result<Expr, ParseError> {
+        let mut expr = self.unary()?;
 
         while self.match_op(vec![TokenType::SLASH, TokenType::STAR]) {
             let op = self.previous();
-            let right = self.unary();
+            let right = self.unary()?;
             expr = Expr::Binary(Box::new(expr), Box::new(op), Box::new(right));
         }
 
-        expr
+        Ok(expr)
     }
     
-    fn unary(&mut self) -> Expr {
+    fn unary(&mut self) -> Result<Expr, ParseError> {
         if self.match_op(vec![TokenType::BANG, TokenType::MINUS]) {
             let op = self.previous();
-            let right = self.unary();
-            return Expr::Unary(Box::new(op), Box::new(right));
+            let right = self.unary()?;
+            return Ok(Expr::Unary(Box::new(op), Box::new(right)));
         }
 
         self.primary()
     }
 
-    fn primary(&mut self) -> Expr {
-        if self.match_op(vec![TokenType::FALSE]) { return Expr::Literal(Literal::Bool(false)); }
-        if self.match_op(vec![TokenType::TRUE]) { return Expr::Literal(Literal::Bool(true)); }
-        if self.match_op(vec![TokenType::NIL]) { return Expr::Literal(Literal::Nil); }
+    fn primary(&mut self) -> Result<Expr, ParseError> {
+        if self.match_op(vec![TokenType::FALSE]) { return Ok(Expr::Literal(Literal::Bool(false))); }
+        if self.match_op(vec![TokenType::TRUE]) { return Ok(Expr::Literal(Literal::Bool(true))); }
+        if self.match_op(vec![TokenType::NIL]) { return Ok(Expr::Literal(Literal::Nil)); }
 
-        if self.match_op(vec![TokenType::NUMBER, TokenType::STRING]) { return Expr::Literal(self.previous().literal); }
+        if self.match_op(vec![TokenType::NUMBER, TokenType::STRING]) { return Ok(Expr::Literal(self.previous().literal)); }
 
         if self.match_op(vec![TokenType::LPAREN]) {
-            let expr = self.expression();
-            self.consume(TokenType::RPAREN, "Expected ')' after expression");
-            return Expr::Grouping(Box::new(expr));
+            let expr = self.expression()?;
+            self.consume(TokenType::RPAREN, "Expected ')' after expression")?;
+            return Ok(Expr::Grouping(Box::new(expr)));
         }
-        self.expression()
+
+        Err(ParseError{ token: self.peek(), message: "Expected expression".to_string() })
     }
 
     fn consume(&mut self, token_type: TokenType, message: &str) -> Result<Token, ParseError> {
         if self.check(token_type) { return Ok(self.advance()); }
 
         Err(ParseError{ token: self.peek(), message: message.to_string() })
+    }
+
+    fn synchronize(&mut self) {
+        self.advance();
+
+        while !self.is_at_end() {
+            if self.previous().token_type == TokenType::SEMICOLON { return; }
+
+            match self.peek().token_type {
+                TokenType::CLASS | TokenType::FOR | TokenType::FUN | 
+                    TokenType::IF | TokenType::PRINT | TokenType::RETURN | 
+                    TokenType::VAR | TokenType::WHILE => return,
+                _ => (),
+            }
+            self.advance();
+        }
     }
 
     fn match_op(&mut self, token_types: Vec<TokenType>) -> bool {
